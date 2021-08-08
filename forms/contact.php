91360aa8ff6748b14aeee35dfd4002163aa44703
KIRAN RAJ DHAKAL <65303319+leyuskckiran1510@users.noreply.github.com>
2021-08-08 12:55:44 +0545

    Update contact.php
    added new mail php

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,27 @@
 <?php
+$to = "me@example.com"; // Your Brand Mail ID
+$from = $_POST['email'] ; // Replace it with your From Mail ID
+
+$headers = "From: " .  $_POST['name'];
+
+$subject = $_POST['subject'];
+$body = "FeedBack:  " .$_POST['message'];
+if( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) )
+{
+if (mail($to, $subject, $body, $headers, "-f " . $from))
+{
+echo 'Your Feedback from (' . $_POST['email'] . ') email has been sent wait for reply !';
+}
+else
+{
+echo 'There was a problem with your e-mail (' . $_POST['email'] . ')';
+}
+}
+?>
+
+///*
+
+<?php
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
@@ -39,3 +62,4 @@
 
   echo $contact->send();
 ?>
+*/
